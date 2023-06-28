5f6a2ecfa1a1b013750f82a43ab270204a7ac4bf
Abhinav Gorantla <abhinav20016@gmail.com>
2023-06-28 19:50:55 +0530

    removed unused mysql config from settings.py file

--- gptrecipes/settings.py
+++ gptrecipes/settings.py
@@ -74,16 +74,17 @@ WSGI_APPLICATION = 'gptrecipes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'recipes',
-        'USER': 'root',
-        'PASSWORD': 'Password123',
-        'HOST': 'localhost',
-        'PORT': '3306'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'recipes',
+#         'USER': 'root',
+#         'PASSWORD': 'Password123',
+#         'HOST': 'localhost',
+#         'PORT': '3306'
+#     }
+# }
+DATABASES = {}
 
 
 # Password validation
