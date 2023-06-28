6df31b2f207aa2985b166189ffbdfed79194c58a
Abhinav Gorantla <abhinav20016@gmail.com>
2023-06-28 19:54:55 +0530

    Added config files for railway

--- website/firebase.py
+++ website/firebase.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import uuid
-cred = credentials.Certificate('website\gpt-recipes-2dc15-firebase-adminsdk-fg9f3-89d6d23793.json')
+cred = credentials.Certificate('website/gpt-recipes-2dc15-firebase-adminsdk-fg9f3-89d6d23793.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 recipe_ref = db.collection("recipes")
